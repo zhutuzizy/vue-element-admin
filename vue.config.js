c0aea1668391e6fb7fa6877f365d9e8f6aa4a567
张也 <hzzhangye@corp.netease.com>
2025-03-04 20:31:40 +0800

    update dist/

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,8 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  // publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/english/' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
