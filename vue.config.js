d6b456652b4f7bcf44fb500d3eb0169e5f8e7a07
张也 <hzzhangye@corp.netease.com>
2025-03-05 23:22:21 +0800

    feat: update config-3.5

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   // publicPath: '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-element-admin/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './vue-element-admin/' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
